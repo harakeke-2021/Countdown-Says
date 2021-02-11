51702c66e6b5f069c5c8078f51e0d73c9b3be529
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-12 12:17:20 +1300

    countdown timer

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Buttons from './Buttons'
+import Timer from './Timer'
 
 export default function Board () {
   return (
     <div>
       <h1 className="title">COUNTDOWN SAYS</h1>
+      <div><Timer /></div>
       <Buttons />
     </div>
   )
