b2eb8df8fb96eb32a62e321d1d87189114b97def
tausani-ah-chong <58315812+tausani-ah-chong@users.noreply.github.com>
2021-02-12 15:14:02 +1300

    add logic to buttons

--- client/components/Buttons.jsx
+++ client/components/Buttons.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Buttons() {
+export default function Buttons () {
+  const winMessage = 'You Won!'
+  const loseMessage = 'You Lose, Start again'
+
+  const [userInput, setUserInput] = useState([])
+
+  const sequence = [2, 1, 3, 1, 4]
+  // const userInput = []
+
+  function clickHandler (e) {
+    const divNum = e.target.id
+    userInput.push(Number(divNum))
+
+    const allGood = userInput.every((num, i) => {
+      return sequence[i] === num
+    })
+
+    if (allGood) {
+      if (userInput.length !== 5) {
+        // alert(carryOnMsg)
+      } else {
+        alert(winMessage)
+        setUserInput([])
+      }
+    } else {
+      alert(loseMessage)
+      setUserInput([])
+    }
+    // if userInput index value !== sequence index value
+    // display loseMessage, change userInput to empty
+    // if userInput length and match == sequence, winMessage
+  }
+
   return (
     <div className="container">
-      <div className="red">
+      <div id='1' className="red" onClick={clickHandler}>
       </div>
 
-      <div className="blue">
+      <div id='2' className="blue" onClick={clickHandler}>
       </div>
 
-      <div className="green">
+      <div id='3' className="green" onClick={clickHandler}>
       </div>
 
-      <div className="pink">
+      <div id='4' className="pink" onClick={clickHandler}>
       </div>
 
     </div>
